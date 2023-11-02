fcbad229df7dbd92bbb39c74e5dea479339d1666
j-m-c-b <25614891+j-m-c-b@users.noreply.github.com>
2023-11-02 15:35:36 -0500

    test3

---
+++ sw.js
@@ -0,0 +1,21 @@
+var GHPATH = '/ProQr';
+ 
+// Choose a different app prefix name
+var APP_PREFIX = 'proQ';
+ 
+// The version of the cache. Every time you change any of the files
+// you need to change this version (version_01, version_02…). 
+// If you don't change the version, the service worker will give your
+// users the old files!
+var VERSION = 'version_00';
+ 
+// The files to make available for offline use. make sure to add 
+// others to this list
+var URLS = [    
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+ // `${GHPATH}/css/styles.css`,
+ // `${GHPATH}/js/app.js`
+  `${GHPATH}/caDupli-master/files/JSON/consult.html`
+  `${GHPATH}/caDupli-master/files/JSON/html5-qrcode.min.js`
+]
