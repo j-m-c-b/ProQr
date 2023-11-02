b0fe79d4894436fba886d1cec5d7a13edd26868a
j-m-c-b <25614891+j-m-c-b@users.noreply.github.com>
2023-11-02 17:35:22 -0500

    yolo

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@ var APP_PREFIX = 'proQ';
 
 
 var APP_PREFIX = 'pqr'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_05'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_08'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [    
   'https://j-m-c-b.github.io/ProQr',                        // Add URL you want to cache in this list.
@@ -27,6 +27,10 @@ var URLS = [
   'https://j-m-c-b.github.io/ProQr/index.html',            // add path to those files here 
   'https://j-m-c-b.github.io/ProQr/caDupli-master/files/JSON/consult.html',
   'https://j-m-c-b.github.io/ProQr/caDupli-master/files/JSON/html5-qrcode.min.js',
+  'https://j-m-c-b.github.io/ProQr/searchjs/flexsearch.compact.js',
+  'https://j-m-c-b.github.io/ProQr/searchjs/JsBarcode.all.min.js',
+  'https://j-m-c-b.github.io/ProQr/searchjs/findProducts.html'
+  
            // If you have separate JS/CSS files,
 ]
 
