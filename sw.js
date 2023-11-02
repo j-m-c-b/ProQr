022c7cdedd193bb015172fd46cfc6baf35cb71c1
j-m-c-b <25614891+j-m-c-b@users.noreply.github.com>
2023-11-02 16:05:59 -0500

    fu this

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var GHPATH = '/ProQr';
+
  
 // Choose a different app prefix name
 var APP_PREFIX = 'proQ';
@@ -12,50 +12,73 @@ var APP_PREFIX = 'proQ';
 // others to this list
 
 
-var VERSION = 'version_003';
+
+
+
+
+
+
+var APP_PREFIX = 'pqr'     // Identifier for this app (this needs to be consistent across every cache update)
+var VERSION = 'version_05'              // Version of the off-line cache (change this value everytime you want to update cache)
+var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [    
-  `${GHPATH}/`,
-  `${GHPATH}/index.html`,
- `${GHPATH}/caDupli-master/files/JSON/consult.html`
- `${GHPATH}/caDupli-master/files/JSON/html5-qrcode.min.js`
+  'https://j-m-c-b.github.io/ProQr',                        // Add URL you want to cache in this list.
+'https://j-m-c-b.github.io/ProQr/',                // If you have separate JS/CSS files,
+  'https://j-m-c-b.github.io/ProQr/index.html',            // add path to those files here 
+  'https://j-m-c-b.github.io/ProQr/caDupli-master/files/JSON/consult.html',
+  'https://j-m-c-b.github.io/ProQr/caDupli-master/files/JSON/html5-qrcode.min.js',
+           // If you have separate JS/CSS files,
 ]
 
-
-var CACHE_NAME = APP_PREFIX + VERSION
+// Respond with cached resources
 self.addEventListener('fetch', function (e) {
-  console.log('Fetch request : ' + e.request.url);
+  console.log('fetch request : ' + e.request.url)
   e.respondWith(
     caches.match(e.request).then(function (request) {
-      if (request) { 
-        console.log('Responding with cache : ' + e.request.url);
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
         return request
-      } else {       
-        console.log('File is not cached, fetching : ' + e.request.url);
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
         return fetch(e.request)
       }
+
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
     })
   )
 })
 
+// Cache resources
 self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      console.log('Installing cache : ' + CACHE_NAME);
+      console.log('installing cache : ' + CACHE_NAME);
+      console.log(URLS);
       return cache.addAll(URLS)
+      .catch(function (error) {
+        console.error('Cache addAll error:', error)
+      });
+      
     })
   )
 })
 
+// Delete outdated caches
 self.addEventListener('activate', function (e) {
   e.waitUntil(
     caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create white list
       var cacheWhitelist = keyList.filter(function (key) {
         return key.indexOf(APP_PREFIX)
       })
-      cacheWhitelist.push(CACHE_NAME);
+      // add current cache name to white list
+      cacheWhitelist.push(CACHE_NAME)
+
       return Promise.all(keyList.map(function (key, i) {
         if (cacheWhitelist.indexOf(key) === -1) {
-          console.log('Deleting cache : ' + keyList[i] );
+          console.log('deleting cache : ' + keyList[i] )
           return caches.delete(keyList[i])
         }
       }))
